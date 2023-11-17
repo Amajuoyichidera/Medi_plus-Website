c9df4e3df633478689f52bebfd35912c59c573cb
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2023-11-17 16:16:27 +0100

    first version

---
+++ components/sectionEight/SectionEight.jsx
@@ -0,0 +1,15 @@
+import { Heart } from "../sectionThree/SectionThree"
+import { FaClipboardList, FaRegEye, FaEar, FaTooth, FaHeart, FaTint } from "react-icons/fa";
+
+const SectionEight = () => {
+  return (
+    <div>
+      <FaEar />
+      <FaTooth />
+      <FaHeart />
+      <FaTint />
+    </div>
+  )
+}
+
+export default SectionEight
