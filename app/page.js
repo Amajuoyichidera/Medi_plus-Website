5f5f198d416ec8d35e4cdefff43f726eab9c23ca
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2023-11-23 10:46:31 +0100

    first version

--- app/page.js
+++ app/page.js
@@ -6,13 +6,13 @@ import SectionThree from '@/components/sectionThree/SectionThree';
 import SectionFour from '@/components/sectionFour/SectionFour';
 import SectionFive from '@/components/sectionFive/SectionFive';
 import SectionSix from '@/components/sectionSix/SectionSix';
-import SectionSeven from '@/components/sectionSeven/sectionSeven';
 import SectionEight from "@/components/sectionEight/SectionEight";
 import SectionNine from "@/components/sectionNine/SectionNine";
 import SectionTen from "@/components/sectionTen/SectionTen";
 import SectionEleven from "@/components/sectionEleven/SectionEleven";
 import SectionTwelve from "@/components/sectionTwelve/SectionTwelve";
 import Footer from "@/components/footer/Footer";
+import SectionSeven from '@/components/sectionSeven/SectionSeven';
 
 export default function Home() {
   const sections = [
